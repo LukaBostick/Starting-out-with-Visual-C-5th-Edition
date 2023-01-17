761ae765f682a3a064759229745b2c0f3008be1c
luka bostick <lukab@LAPTOP-QLVTPE3M>
2023-01-17 15:57:59 -0600

    Add project files.

---
+++ Intro_to_Csharp_Pgm_Luka/Chapter 1 Introduction to Computers and Programming.cs
@@ -0,0 +1,387 @@
+/*
+ * //////////////////////////////////////////////////////////////////////////
+ * Chapter 1 Introduction to Computers and Programming 
+ * //////////////////////////////////////////////////////////////////////////
+ * //////////////////////////////////////////////////////////////////////////
+ *						Topics
+ *						
+ * 1.1 Introduction
+ * 
+ * 1.2 Hardware and Software
+ * 
+ * 1.3 How Computers Store Data
+ * 
+ * 1.4 How a Program Works
+ * 
+ * 1.5 Graphicals User Interfaces
+ * 
+ * 1.6 Objects
+ * 
+ * 1.7 The Program Development Process
+ * 
+ * 1.8 Getting Started with the Visual Studio Environment
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *					1.1 Introduction
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *	Think abt some of the different ways that people use Computers. In school,
+ *	students use computers for tasks such as writting papers, searching for 
+ *	articales, sending e-mail, and participating in online classes. At work,
+ *	people use computers to analyse data, make presentations, conduct business
+ *	transaction, communicate with customers and coworkers, control machines 
+ *	in manufacturing facilities, and do many other things. At home, people use
+ *	computers for tasks such as paybills, shopping online, staying connected
+ *	with firends and family, and playing computer games.
+ *	And don't forget that smart phones, tablets, car navigation system, and ,many
+ *	other devices are computers as well. The uses of computers are almost limitless
+ *	in out everyday lives. Computers can do such a wide variety of things because
+ *	they can be programmed, which means that computers are designed not to do just 
+ *	one job, but to do any job that their programs tell them to do. 
+ *	
+ *	A program is a set of instercutions that a computer follows to perform a task. 
+ *	
+ * //////////////////////////////////////////////////////////////////////////
+ *	
+ *	For ex Figure 1-1
+ *			
+ *			shows screens from two commonly used Microsoft programs:
+ *			Word and PowerPoint. 
+ *			
+ *			Word is a word processing program that allows you to create,
+ *			edit and print documents. PowerPoint allows you to create graphical
+ *			slides and use them as part of a presentation.
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ * Programs are commonly referred to as software. Software is essential to
+ * a computer because without software, a computer can do nothing. All the 
+ * software that makes our computers useful created by individuals know as
+ * programmers, or software developers. A programmer, or software developer,
+ * is a person with the training and skills necessary to design, create and
+ * test computer programs. Computer programming is an exciting and rewarding
+ * career. ( cap) Todays, programmer work in business, medicine, government,
+ * law enforcement, agriculture, academics, entertainment, and almosy every
+ * other field.
+ * 
+ * this book introduces you to the fundamental concepts of computer
+ * programming using the c# programming language. Before we begin exploring 
+ * those concepts, you need to understand a few basic things abt computers
+ * and how they work.
+ * This chapter provides a solid foundation of knowledge that
+ * you will continually rely pm as ypu study computers science. 
+ * 
+ * First, we discuss the physical components that computers are commonly made of.
+ * 
+ * Then, we look at how computers store data execute programs.
+ * Next, we intorduce you to two fundamental elements of modern software design
+ * : graphical user interdaces and objs. Finally we give a quick intorduction to the 
+ * software used to write C# programs.
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *						1.2 Hardware and Software
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *								Concept
+ * //////////////////////////////////////////////////////////////////////////
+ * The physical devices that a computer is made of are referred to as
+ * the computer's hardware. The programs that run on a computer are to as
+ * software.
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *							Hardware
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *			Hardware refers to the physical devices, or components,
+ *		of which a computer is made. A computer is not one single
+ *		device but is a system of devices that all work together. 
+ *		Like the different instruments in a symphony orchestra, each device
+ *		in a computer plays its own part.
+ *		
+ *		If you have ever shopped for a computer, you jave probably seen sales
+ *		literature listing components such as microprocessors, memory, disk 
+ *		drives, video displays, graphics card, and so fourth. 
+ *		
+ *		Unless you already know a lot abt computers or at least have a firend
+ *		who does,understanding what these different components do can be
+ *		confusing.
+ *		
+ *		systems consists of the folloiwing major components:
+ *		
+ *		*The central processing unit (CPU)
+ *		
+ *		*Main memory
+ *		
+ *		*Secondary storagr devices
+ *		
+ *		* Input devices
+ *		
+ *	LEts take a closer look at each of these components
+ *	
+ * //////////////////////////////////////////////////////////////////////////
+ *                              The CPU             
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * When a computer is performing the task that a program tells it to do, we
+ * 
+ * say that computer is running or executing the program. The Centeral proc
+ * essing unit, or CPU, is the part of a computer that runs the prgrams.
+ * 
+ * The CPU is the most important component in a computer because without
+ * it the computer could not run software.
+ * 
+ * IN the earliest computers, CPUs were huge devices made of electrical
+ * and mechanical components such as vauum tubes and switches.
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *                  Figure 1-3 shows such a device.  
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * The two women in the photo are working with the historic ENIAC computer. 
+ * 
+ * The ENIAC, considered by many to be the world's first programmable
+ * electronic computer, was built in 1945 to calculate artillery ballistic
+ * tables for the U.S.Army. This machine, which wasx primarily one big CPU,
+ * was 8 feet tall and 100 feet long and weighed 30 tons.
+ * 
+ * Today, CPUs are small chips known as microprocessors.
+ * Figure 1-4 shows a photo of a lab technician holding a modern-day
+ * microprocessor. Inadditioon to being much smaller than the old
+ * electromechanical CPUs in early computers.
+ * microprocessors are also much more powerful.
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *        Figure 1-4 A lab tecnician holds a modern microprocessor
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *                      Main Memory
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * You can think of main memory ad the computer's work area.
+ * This is where the computer stores a program while the program
+ * is running, as well as the data that the program is working with.
+ * For ex, suppose you are using word processing program to write an essay
+ * for one of your classes. While you do this, both the wrod processing
+ * program and the essay are stored in main memory.
+ * 
+ * 
+ * Main memory is commonly know as random access memory, or RAM.
+ * It is called this because the CPU is able to quickly access data stored at
+ * any random location in RAM.
+ * RAM is usually a volatile type of memory that is used only for temporary
+ * storage while a program is running.
+ * When the computer is turned off, the contents of RAM are eraded.
+ * Inside your computer, RAM is stored in chips,
+ * similary to ones shown in Figure 1-5.
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *                              Secondary Storage Devices
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * 
+ * Secondary storafe is a type of memory that can hold data for long periods
+ * of time, even when there is no power to the computer.
+ * Profgrams are normally stored in sdecondary memory and loaded into main
+ * memory as needed. Important data, such as word processing documents, payroll
+ * data, and inventory records, is saved to decondary storage as well.
+ * 
+ * The most common type of secondary storage device is the disk drive.
+ * A traditional disk drive stores data by magneticallty encoding it onto
+ * a spinning ciricual disk. Solid-state drives, which store data in
+ * solid-state memory, are increasingly becoming popualar.
+ * A solid-state memory, are increasingly becoming populaer.
+ * A solid-state srive has no moving psarts and operstes faster than a 
+ * traditional disk drive
+ * 
+ * Most computers have some sort of secondary storage device, either a 
+ * traditional disk drive or a solid-state drive, mounted inside their case
+ * External storage devices are also available, shice connect to one of
+ * the computer's communucation ports,or plug into a memory slot.
+ * Exteranl storage devices can be uised to crate backups copies or
+ * important data or to move data to another computer.
+ * 
+ * For example, USB (Universal Serial Bus) drives anmd SD (Secure Digutal)
+ * memory cards are small devices that appear in the system as disk drives.
+ * They are inexpensive, reliable,
+ * and small enough to be carried in your pocket.
+ * 
+ * Optical devices such as the compact disk(CD) and the digital versatile
+ * disk (DVD) are also popular for data storage.
+ * Data is not recorded magtically on a optical disk but is encoded
+ * as a series of pits and thus read the encoded data, Optical dics
+ * hold large amounts of data, and bacaues recordable CD and DVD
+ * drives are now commonplace, they are good mediums
+ * for crating backopcopies of data.
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *                          Input Devices
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * Input is any data the computer collects form people and from other devices. 
+ * 
+ * The conponent that collects the data and sends
+ * it to the computer is called and input device.
+ * 
+ * Common input devices are the keyboard,
+ * mouse, touchscreen, scanner, mictophone, and digital camera. 
+ * 
+ * Disk drives and optical drives can also be considered input
+ * devices because programs and data are retriceed from them and
+ * loaded into the computer's memory.
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *                              Output Devices
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ *Output is any data the computer prodices for people or for other devices
+ *it meight be a sales report, a list of names, or a graphic image.
+ *
+ *the data is sent to an output devices, which formates and presentrs it.
+ *
+ *Common output devices are screens and printers.
+ *Storage decices can also be condidered oputput devices bnecause the system
+ *sends data the them in order to be saved.
+ *
+ * //////////////////////////////////////////////////////////////////////////
+ *                                 Software
+ * //////////////////////////////////////////////////////////////////////////
+ *
+ *  IF a computer is to function, softwere is not optional.
+ *  Everything that a computer does, from the time you turn
+ *  the power switch on until you shut the system down, is under the contro
+ *  l of software.
+ *  There are two general catogries of software:
+ *  system software and application software.
+ *  Most computers programs clearly fit into ine if these two catergories.
+ *  Let's take a closer look at each.
+ *  
+ *  
+ *  //////////////////////////////////////////////////////////////////////////
+ *                                 System Software
+ *  //////////////////////////////////////////////////////////////////////////
+ 
+ *  The programs that control and manage the basic operations of a computer are 
+ *  generally reffered to as system software. System software typically includes
+ *  
+ *  the folloeing types of program:
+ *  
+ *  //////////////////////////////////////////////////////////////////////////
+ *                              Opersating Systems
+ *  //////////////////////////////////////////////////////////////////////////
+ *  
+ *  An operating system is the most fundamental set od programs on a computer.
+ *  
+ *  The operating systems controls the internal operations of the computer's
+ *  
+ *  hardware, manages all the deives connected to the computer, allows other 
+ *  programs to run on the computer.
+ *  
+ *  //////////////////////////////////////////////////////////////////////////
+ *                              Utility Prgrams
+ *  //////////////////////////////////////////////////////////////////////////
+ *  A untility programs perfoems a specialized task that enhances the computer's
+ *  
+ *  operation or safegauards data. 
+ *  Examples of utility programs are virus scanners, files-compression
+ *  programs, data-back programs.
+ * 
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *                          Software Development Tools
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * The software tools that programmers use to create, modify, and test
+ * software are refered to as software development tools.
+ * 
+ * Assemblers, compilers, and interperters, which are discussed later in this ch.
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *                         Application Softeare     
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * 
+ * Programs that make a computer useful for everyday tasks are know as
+ * application software.These are the programs tha
+ * t people normally spend most if their time running on their compuers.
+ * 
+ * Fig 1-1 shows screen from two commonly used application-Microrsoft Word,
+ * a word processing program, and Mictrosoft Powerpoint, a presentation prgram. 
+ * 
+ * Some other examples of appllication software are spreadsheets programs, e-mail
+ * programs, web browsers, and game programs.
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *                          1.3 How Computers Store Data
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *              Concept:
+ *       All data stored in a computer is converted to sequences of 0a and 1s.
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * A computer's memory is divided into tinny storage location known as bytes. 
+ * One byte is enought memory to store only a letter the alphabet ot a small number. 
+ * 
+ * In order to do anything meaningful, a computer has to have lots of bytes.
+ * Most computers today have millions, or even billions, of bytes of memory.
+ * 
+ * Each byte is divided into eight smaller storage location known as bits.
+ * 
+ * The term bit stans for binday digit. Computer scientists usually think of bits
+ * as tiny switches that can either on or off. Bits arent't actual "switches,"
+ * however, at least not in the conventiuonal senes. Inmost computer systems,unj
+ * bits are tiny elecetircal compionts that can hold wityher
+ * a positiove or neagice charge. Computers scientists think of a positicve
+ * charge as a switch in the on position and a neguitve charfge as a asichv 
+ * in the pff position> Figure 1-6 shows the way that a compuyer scientist
+ * might think of a  byte of memory: as collection of switiches what are
+ * flipped to either the on ir the off position.
+ * 
+ * When a piece of data is stored in a byte, the computer sets the eights
+ * nits to an on/off pattern that represents the data. For ex, the pattern
+ * whon on the left in Figure 1-7 showsshows the number 77 woulf be sotred
+ * in a byte. in a moment, you will see hopw these patters are determined.
+ * 
+ * 
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ *                              Storing Numbers
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ * A bit can be used in a very limted way to represent numbers.
+ * 
+ * Depending on whether the bit is turned on or off, it can represnet
+ * one of two different the number 0 and bit that is turned on represents the num 1.
+ * 
+ * This corrensponds perfectly to the binwary numbering system. In the binary
+ * numbering system ( or binary, as it is usually called), all
+ * numerica values are written as sequences of 0s and 1s. 
+ * Here is an example of a number that us written in binary
+ * 
+ * The position of wach digit in a binary number has a value assigned to it. 
+ * 
+ * Starting with the rightmost digit andf moving left, the posotion vcalue are
+ * 2^0,2^1,2^2,2^3, and so forth, as shown in Figure 1-8. Figure 1-9 shows
+ * the same diagram with the position values calculated.
+ * 
+ * Starting with the rightmost digit and moving left,
+ * the position values are 1,2,4,8, and so forth.
+ * 
+ * 
+ * 
+ * //////////////////////////////////////////////////////////////////////////
+ */
---
+++ Intro_to_Csharp_Pgm_Luka/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+/** 
+ * //////////////////////////////////////////////////////////////////////////
+ * 
+ *              Starting ouit with Visual C#
+ * This is the eBook of the printed book and may not include any media, website
+ * access acodes, or print supplements that may come packled with the bounded book. 
+ * 
+ * For courses in introductory c# programming. Motivate students with clear,
+ * down-to-earth explanations andfamiliar grapgical elements Starting out
+ * With Visual C# is an ideal intorductory Visual C# text for students with
+ * no prior programming experience.
+ * Students who are new to programming will appreciate the clear, down-to-earth
+ * explanations and the detailed walk-throughs that are provided by the hands
+ * -on tutorials. Gaddis's halmartk, setp-by-step instructions are supported by a
+ * 
+ * GUI-based approach that motivates students
+ * 
+ * Sold By: Person
+ * 
+ * ISBNs:
+ * 09780134400433, 9780135183519, A102000018387, 0135183510,
+ * 9780135183519, 013520481X, 9780135204818
+ * 
+ *  Number of Pages: 960
+    Edition: 5th
+    File Type: EPUB
+ * //////////////////////////////////////////////////////////////////////////
+ */
